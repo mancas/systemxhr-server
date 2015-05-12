c3b8e1d66c45a6c698e0507169909d6ed10964b2
Manuel <manuel.casasbarrado@gmail.com>
2015-05-12 11:21:48 +0200

    minor issue

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@
     function _cloneObject(obj) {
       var cloned = {};
       for (var key in obj) {
-        console.info(typeof obj[key], key);
+        console.info(typeof obj[key] !== 'object' || typeof obj[key] !== 'function', key);
         if (typeof obj[key] !== 'object' || typeof obj[key] !== 'function') {
           cloned[key] = obj[key];
         }
